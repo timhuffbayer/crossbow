df8278e0443d753755ae806648bbf9f9a21ebc04
Jon Zumbrun <jezum@icloud.com>
2024-12-15 21:36:17 -0600

    uodate button

--- components/button/Button.js
+++ components/button/Button.js
@@ -3,6 +3,14 @@ import css from '../../lib/css.js'
 
 export default function Button({ onClick, color = '', title = '' }) {
   
-  const cssClass = css`.${color ? 'button-' + color : 'button'} { background-color: ${color}; }`
-  return html`<button class="${cssClass}" @click="${() => onClick()}">${title}</button>`
+  const cssClass = css`
+    .${color ? 'button-' + color : 'button'} { 
+      background-color: ${color};
+    }
+  `
+  return html`
+    <button class="${cssClass}" @click="${() => onClick()}">
+      ${title}
+    </button>
+  `
 }

ac18358423c8d440aa17a370dd56cabfa4815bd0
Jon Zumbrun <jezum@icloud.com>
2024-12-16 14:22:45 -0600

    Update App.js

--- App.js
+++ App.js
@@ -8,6 +8,11 @@ import AboutPage from './pages/about/AboutPage.js'
 export default function App() {
   const state = reactive({ page: ''})
 
+  // For github pages
+  if(location.pathname.includes('quiver')) {
+    page.base('/quiver')
+  }
+
   page.redirect('/index.html', '/')
   page('/', () => {
     state.page = Layout(HomePage, { title: 'Home' })

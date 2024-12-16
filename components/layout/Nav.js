15a197581ae41bb951fc1a1a1e2ac4bb7a36f466
Jon Zumbrun <jezum@icloud.com>
2024-12-16 14:32:51 -0600

    Update Nav.js

--- components/layout/Nav.js
+++ components/layout/Nav.js
@@ -1,14 +1,15 @@
 import { html } from 'https://esm.sh/@arrow-js/core'
+import page from 'https://esm.sh/page'
 import { layoutStore, login } from './LayoutStore.js'
 
 export default function Nav() {
-    
+
   return html`
   <nav>
     <ul>
     <li><strong>Quiver</strong></li>
-      <li> <a href="/">Home</a> </li>
-      <li> <a href="/about">About</a> </li>
+      <li> <a href="javascript:void(0)" @click="${page('/')}">Home</a> </li>
+      <li> <a href="javascript:void(0)" @click="${page('/about')}">About</a> </li>
     </ul>
     <ul>
       <li> <a href="javascript:void(0)" @click="${() => login()}">${() => layoutStore.loggedIn ? 'Logout' : 'Login'}</a> </li>
